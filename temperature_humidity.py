2a18d1419375acfe1bd9a6ab35527eaaef23f167
Junosprite007 <Junosprite007@gmail.com>
2023-02-26 18:30:35 -0800

    added fahrenheit.

--- temperature_humidity.py
+++ temperature_humidity.py
@@ -6,10 +6,11 @@ DHT_PIN = 4
 
 printTimer = 0
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    humidity, temperatureC = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    temperatureF = temperatureC * (9 / 5) + 32
 
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    if humidity is not None and temperatureC is not None:
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperatureC, humidity))
     else:
         print("Failed to retrieve data from humidity sensor")
     time.sleep(1)
